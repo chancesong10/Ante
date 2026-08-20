8a8af74c35b4afaa57402dff6734597292649fc0
Chance Song <chancesong10@gmail.com>
2026-08-20 12:27:58 -0700

    created tests to confirm validity of insights as well as a audit

---
+++ babel.test.config.js
@@ -0,0 +1,6 @@
+// Babel config used ONLY by Jest (see jest.config.js). Kept separate from
+// Metro's bundling pipeline, which has no babel.config.js of its own and
+// should not be affected by adding a test setup.
+module.exports = {
+  presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
+};
